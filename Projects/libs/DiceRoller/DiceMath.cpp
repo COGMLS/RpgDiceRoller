24b3099f86aa19adebd496c83af34b08aab6edb4
COGMLS <mlsmatheus@hotmail.com>
2025-04-24 23:06:08 -0300

    Added DiceMath.hpp and DiceMath.cpp

---
+++ Projects/libs/DiceRoller/DiceMath.cpp
@@ -0,0 +1,427 @@
+#include "DiceMath.hpp"
+
+#ifdef DICE_ROLLER_ENABLE_EXPERIMENTAL_DICE_MATH
+
+//
+// DiceMathCmd:
+//
+
+DiceMathCmd::DiceMathCmd()
+{
+	this->type = DiceMathType::NOT_DEFINED;
+}
+
+DiceMathCmd::DiceMathCmd(const DiceMathCmd &other)
+{
+	this->type = other.type;
+	this->data = other.data;
+}
+
+DiceMathCmd::DiceMathCmd(DiceMathCmd &&other) noexcept
+{
+	this->type = std::move(other.type);
+	this->data = std::move(other.data);
+}
+
+DiceMathCmd::~DiceMathCmd()
+{
+	
+}
+
+DiceMathCmd &DiceMathCmd::operator=(const DiceMathCmd &other)
+{
+	this->type = other.type;
+	this->data = other.data;
+	return *this;
+}
+
+DiceMathCmd &DiceMathCmd::operator=(DiceMathCmd &&other) noexcept
+{
+	if (this == &other)
+	{
+		return *this;
+	}
+	
+	this->type = std::move(other.type);
+	this->data = std::move(other.data);
+	return *this;
+}
+
+int DiceMathCmd::getValue()
+{
+	return 0;
+}
+
+DiceMathType DiceMathCmd::getType()
+{
+	return DiceMathType::NOT_DEFINED;
+}
+
+//
+// DiceMathDiceObj:
+//
+
+DiceMathDiceObj::DiceMathDiceObj(Dice dice)
+{
+	this->type = DiceMathType::DICE_MATH;
+	this->data = DiceMathStore(dice);
+}
+
+DiceMathDiceObj::DiceMathDiceObj(std::vector<Dice> dices)
+{
+	this->type = DiceMathType::DICE_MATH;
+	this->data = DiceMathStore(dices);
+}
+
+DiceMathDiceObj::DiceMathDiceObj(DiceType dice, unsigned int nDices)
+{
+	this->type = DiceMathType::DICE_MATH;
+	this->data = DiceMathStore(dice, nDices);
+}
+
+DiceMathDiceObj::DiceMathDiceObj(const DiceMathDiceObj &other)
+{
+	this->data = other.data;
+	this->type = other.type;
+}
+
+DiceMathDiceObj::DiceMathDiceObj(DiceMathDiceObj &&other) noexcept
+{
+	this->data = std::move(other.data);
+	this->type = std::move(other.type);
+}
+
+DiceMathDiceObj::~DiceMathDiceObj()
+{
+}
+
+DiceMathDiceObj &DiceMathDiceObj::operator=(const DiceMathDiceObj &other)
+{
+    this->data = other.data;
+	this->type = other.type;
+	return *this;
+}
+
+DiceMathDiceObj &DiceMathDiceObj::operator=(DiceMathDiceObj &&other) noexcept
+{
+    if (this == &other)
+	{
+		return *this;
+	}
+
+	this->data = std::move(other.data);
+	this->type = std::move(other.type);
+	return *this;
+}
+
+int DiceMathDiceObj::getValue()
+{
+	int val = 0;
+	if (this->data.getDiceObj() != nullptr)
+	{
+		for (size_t i = 0; i < this->data.getDiceObj()->size(); i++)
+		{
+			val += this->data.getDiceObj()->at(i).getValue();
+		}
+	}
+    return val;
+}
+
+DiceMathType DiceMathDiceObj::getType()
+{
+    return this->type;
+}
+
+//
+// DiceMathAttr:
+//
+
+DiceMathAttr::DiceMathAttr(int value)
+{
+	this->data = DiceMathStore(value);
+	this->type = DiceMathType::NUMBER_TYPE;
+}
+
+DiceMathAttr::DiceMathAttr(const DiceMathAttr &other)
+{
+	this->data = other.data;
+	this->type = other.type;
+}
+
+DiceMathAttr::DiceMathAttr(DiceMathAttr &&other) noexcept
+{
+	this->data = std::move(other.data);
+	this->type = std::move(other.type);
+}
+
+DiceMathAttr::~DiceMathAttr()
+{
+}
+
+DiceMathAttr &DiceMathAttr::operator=(const DiceMathAttr &other)
+{
+    this->data = other.data;
+	this->type = other.type;
+	return *this;
+}
+
+DiceMathAttr &DiceMathAttr::operator=(DiceMathAttr &&other) noexcept
+{
+    if (this == &other)
+	{
+		return *this;
+	}
+
+	this->data = std::move(other.data);
+	this->type = std::move(other.type);
+	return *this;
+}
+
+int DiceMathAttr::getValue()
+{
+    if (this->data.getAttr() == nullptr)
+	{
+		return 0;
+	}
+
+	return *this->data.getAttr();
+}
+
+DiceMathType DiceMathAttr::getType()
+{
+    return this->type;
+}
+
+//
+// DiceMathOp:
+//
+
+DiceMathOp::DiceMathOp(char op)
+{
+	this->op = op;
+	this->type = DiceMathType::MATH_OPERATOR;
+	this->hasCmd = false;
+}
+
+DiceMathOp::DiceMathOp(char op, DiceMathAttr attr)
+{
+	this->op = op;
+	this->type = DiceMathType::MATH_OPERATOR;
+	this->hasCmd = true;
+	this->rCmd = attr;
+}
+
+DiceMathOp::DiceMathOp(char op, DiceMathDiceObj dice)
+{
+	this->op = op;
+	this->type = DiceMathType::MATH_OPERATOR;
+	this->hasCmd = true;
+	this->rCmd = dice;
+}
+
+DiceMathOp::DiceMathOp(const DiceMathOp &other)
+{
+	this->op = other.op;
+	this->type = other.type;
+	this->hasCmd = other.hasCmd;
+	if (other.hasCmd)
+	{
+		this->rCmd = other.rCmd;
+	}
+}
+
+DiceMathOp::DiceMathOp(DiceMathOp &&other) noexcept
+{
+	this->op = std::move(other.op);
+	this->type = std::move(other.type);
+	this->hasCmd = std::move(other.hasCmd);
+	this->rCmd = std::move(other.rCmd);
+}
+
+DiceMathOp::~DiceMathOp()
+{
+
+}
+
+DiceMathOp &DiceMathOp::operator=(const DiceMathOp &other)
+{
+    this->op = other.op;
+	this->type = other.type;
+	this->hasCmd = other.hasCmd;
+	if (other.hasCmd)
+	{
+		this->rCmd = other.rCmd;
+	}
+	return *this;
+}
+
+DiceMathOp &DiceMathOp::operator=(DiceMathOp &&other) noexcept
+{
+	if (this == &other)
+	{
+		return *this;
+	}
+
+	this->op = std::move(other.op);
+	this->type = std::move(other.type);
+	this->hasCmd = std::move(other.hasCmd);
+	this->rCmd = std::move(other.rCmd);
+	return *this;
+}
+
+int DiceMathOp::getValue()
+{
+	if (!this->hasMathCmd())
+	{
+		return 0;
+	}
+
+	return this->rCmd.getValue();
+}
+
+bool DiceMathOp::hasMathCmd()
+{
+    return this->hasCmd;
+}
+
+char DiceMathOp::getOperator()
+{
+    return this->op;
+}
+
+DiceMathType DiceMathOp::getType()
+{
+    return DiceMathType::MATH_OPERATOR;
+}
+
+DiceMath::DiceMath()
+{
+	this->op.push_back(DiceMathOp('+', d20()));
+	this->op.push_back(DiceMathOp('+', 15));
+	this->op.push_back(DiceMathOp('-', d10()));
+	this->op.push_back(DiceMathOp('+', 5));
+
+	for (size_t i = 0; i < this->op.size(); i++)
+	{
+		this->list.push_back(this->op[i]);
+	}
+}
+
+DiceMath::~DiceMath()
+{
+	
+}
+
+DiceMathOp &DiceMath::front()
+{
+	return this->list.front();
+}
+
+DiceMathOp &DiceMath::back()
+{
+    return this->list.back();
+}
+
+void DiceMath::insert(char op, int attr)
+{
+	this->list.push_back(DiceMathOp(op, DiceMathAttr(attr)));
+}
+
+void DiceMath::insert(char op, Dice dice)
+{
+	this->list.push_back(DiceMathOp(op, DiceMathDiceObj(dice)));
+}
+
+void DiceMath::insert(char op, std::vector<Dice> dices)
+{
+	this->list.push_back(DiceMathOp(op, DiceMathDiceObj(dices)));
+}
+
+void DiceMath::insert(char op, DiceType dice, unsigned int nDices)
+{
+	this->list.push_back(DiceMathOp(op, DiceMathDiceObj(dice, nDices)));
+}
+
+DiceMathOp *DiceMath::getOp(size_t pos)
+{
+	if (pos >= this->list.size() || this->list.empty())
+	{
+		return nullptr;
+	}
+
+	return (&this->list.front() + pos);
+}
+
+size_t DiceMath::size()
+{
+    return this->list.size();
+}
+
+int DiceMath::calc()
+{
+	int val = 0;
+
+	size_t i = 0;
+
+	DiceMathOp* cmd = nullptr;
+	DiceMathOp* last = nullptr;
+
+	for (i; i < this->list.size(); i++)
+	{
+		cmd = this->getOp(i);
+
+		if (i != 0)
+		{
+			last = this->getOp(i - 1);
+		}
+
+		DiceMathAttr x = calcDiceCmd(last, cmd);
+
+		val = x.getValue();
+
+		this->list.assign(i, DiceMathOp(cmd->getOperator(), x));
+	}
+
+    return val;
+}
+
+DiceMathAttr calcDiceCmd(DiceMathOp *lastOp, DiceMathOp *op)
+{
+    int val = 0;
+	
+	if (op != nullptr)
+	{
+		if (lastOp == nullptr || !op->hasMathCmd())
+		{
+			return DiceMathAttr(op->getValue());
+		}
+		
+		int lval = 0;
+		int rval = 0;
+
+		lval = lastOp->getValue();
+		rval = op->getValue();
+
+		switch (op->getOperator())
+		{
+			case '+':
+			{
+				val = lval + rval;
+				break;
+			}
+			case '-':
+			{
+				val = lval - rval;
+				break;
+			}
+			default:
+			{
+				break;
+			}
+		}
+	}
+	
+	return DiceMathAttr(val);
+}
+
+#endif // !DICE_ROLLER_ENABLE_EXPERIMENTAL_DICE_MATH
---
+++ Projects/libs/DiceRoller/DiceMath.hpp
@@ -0,0 +1,208 @@
+#pragma once
+
+#ifndef DICE_MATH_HPP
+#define DICE_MATH_HPP
+
+#ifdef WIN32
+	#ifdef DICE_ROLLER_LIB_EXPORTS
+		#define DICE_ROLLER_LIB_API __declspec(dllexport)
+	#else
+		#define DICE_ROLLER_LIB_API __declspec(dllimport)
+	#endif //!DICE_ROLLER_LIB_EXPORTS
+
+	#pragma warning (disable : 4251)
+	#pragma warning (disable : 4273)
+#else
+	#if __GNUC__ >= 4
+		#ifdef DICE_ROLLER_LIB_EXPORTS
+			#define DICE_ROLLER_LIB_API __attribute__((visibility("default")))
+		#else
+			#define DICE_ROLLER_LIB_API __attribute__((visibility("default")))
+		#endif //!DICE_ROLLER_LIB_EXPORTS
+	#else
+		#ifdef DICE_ROLLER_LIB_EXPORTS
+			#define DICE_ROLLER_LIB_API
+		#else
+			#define DICE_ROLLER_LIB_API
+		#endif //!DICE_ROLLER_LIB_EXPORTS
+	#endif
+#endif // !WIN32
+
+#include "ExperimentalFeatures.hpp"
+
+#ifdef DICE_ROLLER_ENABLE_EXPERIMENTAL_DICE_MATH
+
+#include <vector>
+#include <memory>
+#include <list>
+#include <string>
+#include <cstring>
+#include <cmath>
+
+#include "Types.hpp"
+#include "DiceBase.hpp"
+#include "Dices.hpp"
+#include "d100Combined.hpp"
+
+#ifdef DICE_ROLLER_ENABLE_EXPERIMENTAL_DICE_MATH_STORE
+#include "DiceMathStore.hpp"
+#endif // !DICE_ROLLER_ENABLE_EXPERIMENTAL_DICE_MATH_STORE
+
+/**
+ * @brief Dice Mathematical Command base class to provide a inherence to DiceMathAttr, DiceMathDiceObj and DiceMathOp
+ */
+class DiceMathCmd
+{
+	protected:
+
+		DiceMathType type;
+		DiceMathStore data;
+
+	public:
+
+		DiceMathCmd();
+
+		DiceMathCmd (const DiceMathCmd& other);
+		DiceMathCmd (DiceMathCmd&& other) noexcept;
+		
+		~DiceMathCmd();
+
+		DiceMathCmd& operator= (const DiceMathCmd& other);
+		DiceMathCmd& operator= (DiceMathCmd&& other) noexcept;
+
+		virtual int getValue();
+		virtual DiceMathType getType();
+};
+
+/**
+ * @brief Dice Mathematical component for Dice objects
+ */
+class DiceMathDiceObj : public DiceMathCmd
+{
+	public:
+	
+		DiceMathDiceObj (Dice dice);
+		
+		DiceMathDiceObj (std::vector<Dice> dices);
+
+		DiceMathDiceObj (DiceType dice, unsigned int nDices);
+		
+		DiceMathDiceObj (const DiceMathDiceObj& other);
+		
+		DiceMathDiceObj (DiceMathDiceObj&& other) noexcept;
+		
+		~DiceMathDiceObj();
+
+		DiceMathDiceObj& operator= (const DiceMathDiceObj& other);
+		DiceMathDiceObj& operator= (DiceMathDiceObj&& other) noexcept;
+		
+		int getValue();
+
+		DiceMathType getType();
+};
+
+/**
+ * @brief Dice Mathematical Attributes or Bonus holding integers
+ */
+class DiceMathAttr : public DiceMathCmd
+{
+	public:
+	
+		DiceMathAttr (int value);
+
+		DiceMathAttr (const DiceMathAttr& other);
+
+		DiceMathAttr (DiceMathAttr&& other) noexcept;
+
+		~DiceMathAttr();
+
+		DiceMathAttr& operator= (const DiceMathAttr& other);
+		DiceMathAttr& operator= (DiceMathAttr&& other) noexcept;
+
+		int getValue();
+
+		DiceMathType getType();
+};
+
+/**
+ * @brief Dice Mathematical Operator to create the relations between DiceMathAttr or DiceMathDiceObj. The relation is with last operator's value stored applied the mathematical operation between the current operator data stored.
+ */
+class DiceMathOp : public DiceMathCmd
+{
+	private:
+
+		char op;
+		bool hasCmd;
+		DiceMathCmd rCmd;
+	
+	public:
+
+		DiceMathOp (char op);
+
+		DiceMathOp (char op, DiceMathAttr attr);
+
+		DiceMathOp (char op, DiceMathDiceObj dice);
+
+		DiceMathOp (const DiceMathOp& other);
+
+		DiceMathOp (DiceMathOp&& other) noexcept;
+
+		~DiceMathOp();
+
+		DiceMathOp& operator= (const DiceMathOp& other);
+		DiceMathOp& operator= (DiceMathOp&& other) noexcept;
+
+		int getValue();
+		
+		bool hasMathCmd();
+		
+		char getOperator();
+
+		DiceMathType getType();
+};
+
+class DiceMath
+{
+	private:
+	
+		std::vector<DiceMathOp> op;
+		std::list<DiceMathOp> list;	// Doubly linked list of DiceMathOp creating the relationship between the math operators and attributes and dice objects
+
+	public:
+
+		DiceMath();
+
+		//DiceMath (std::string cli);
+
+		~DiceMath();
+
+		DiceMathOp& front();
+
+		DiceMathOp& back();
+
+		void insert (char op, int attr);
+
+		void insert (char op, Dice dice);
+
+		void insert (char op, std::vector<Dice> dices);
+
+		void insert (char op, DiceType dice, unsigned int nDices);
+
+		DiceMathOp* getOp (size_t pos);
+
+		size_t size();
+
+		int calc();
+};
+
+/**
+ * @brief Calculate the dice cmd, using the last operator value with the correct mathematical relation with current operator's value and return a DiceMathAttr to replace the current operator's value with new attribute. If the last operator is nullptr or current operator does not hold a DiceMathCmd, the function will return the current operator's value.
+ * @param lastOp Last operator pointer
+ * @param op Current operator pointer
+ * @return Calculated value between lastOp and op values as a DiceMathAttr.
+ */
+DiceMathAttr calcDiceCmd (DiceMathOp* lastOp, DiceMathOp* op);
+
+#endif // !DICE_ROLLER_ENABLE_EXPERIMENTAL_DICE_MATH
+
+#endif // !DICE_MATH_HPP
