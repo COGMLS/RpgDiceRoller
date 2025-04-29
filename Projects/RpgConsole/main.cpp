f316645e47c85d85ce2f13050e80ede4d8561045
COGMLS <mlsmatheus@hotmail.com>
2025-04-29 10:51:09 -0300

    Removed incompatible code

--- Projects/RpgConsole/main.cpp
+++ Projects/RpgConsole/main.cpp
@@ -65,14 +65,7 @@ int main (int argc, const char* argv[])
 	}
 
 	#ifdef DICE_ROLLER_ENABLE_EXPERIMENTAL_DICE_MATH
-	std::cout << "DiceMathCmd: " << DiceMathCmd().getValue() << " | DiceMathType: " << DiceMathCmd().getType() << std::endl;
-	std::cout << "DiceMathAttr: " << DiceMathAttr(10).getValue() << " | DiceMathType: " << DiceMathAttr(10).getType() << std::endl;
-	std::cout << "DiceMathDiceObj: " << DiceMathDiceObj(d20()).getValue() << " | DiceMathType: " << DiceMathDiceObj(d20()).getType() << std::endl;
-	std::cout << "DiceMathOp: " << DiceMathOp('+', DiceMathAttr(15)).getValue() << " | DiceMathType: " << DiceMathOp('+', 15).getType() << std::endl;
 	
-	DiceMath math;
-	int val = math.calc();
-	std::cout << "DiceMath calc: " << val << std::endl;
 	#endif // !DICE_ROLLER_ENABLE_EXPERIMENTAL_DICE_MATH
 
 	return 0;
